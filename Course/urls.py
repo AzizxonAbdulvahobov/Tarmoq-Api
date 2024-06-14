a5321117bc27fc105943e849c8d1792bd9f65825
AzizxonAbdulvahobov <azizxonabdulvahobov08@gmail.com>
2024-06-14 13:59:23 +0100

    second commit

--- Course/urls.py
+++ Course/urls.py
@@ -43,9 +43,13 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
+
     path('api/v1/auth', include('rest_framework.urls')),
+
     path('api/v1/register/', include('djoser.urls')),
     path('api/v1/token/', include('djoser.urls.authtoken')),
+    
     path('api/v1/', include('main.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 
+from rest_framework import permissions
 
 from django.conf import settings
 from django.core.mail import send_mail
@@ -21,24 +22,28 @@ class CourseAPIViewSet(ModelViewSet):
     """Course API ViewSet: To get, create, update and delete course data"""
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class TeacherAPIViewSet(ModelViewSet):
     """Teacher API ViewSet: To get, create, update and delete teacher data"""
     queryset = models.Teacher.objects.all()
     serializer_class = serializers.TeacherSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class StartCourseAPIViewSet(ModelViewSet):
     """Start Course API ViewSet: To get, create, update and delete started course data"""
     queryset = models.StartCourse.objects.all()
     serializer_class = serializers.StartCourseSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class LessonAPIViewSet(ModelViewSet):
     """Lesson API ViewSet: To get, create, update and delete lesson data"""
     queryset = models.Lesson.objects.all()
     serializer_class = serializers.LessonSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 
@@ -46,6 +51,7 @@ class LessonVideoAPIViewSet(ModelViewSet):
     """Dars videosi API ViewSet: Dars videosi ma'lumotlarini olish, yaratish, yangilash va o'chirish uchun"""
     queryset = models.LessonVideo.objects.all()
     serializer_class = serializers.LessonVideoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CommentViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -53,6 +59,7 @@ class CommentViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Ge
     (no option to update and delete, because I thought such capabilities should not be in annotation)"""
     queryset = models.Camment.objects.all()
     serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class Filter(APIView):
@@ -61,9 +68,11 @@ class Filter(APIView):
         word = str(request.query_params.get('word'))
         lessons = models.Lesson.objects.filter(name__icontains=word)
         return Response({'lessons': serializers.LessonSerializer(lessons, many=True).data})
+    permission_classes = [permissions.IsAuthenticated]
     
 
 class LikeAPIview(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     """Like APIView: To like or dislike lessons"""
     """GET request: Like to get the serializer (not the main data)"""
     def get(self, request):
@@ -80,6 +89,7 @@ class LikeAPIview(APIView):
             value = False
         lesson_id = serializer.validated_data.get('lesson')
         lesson = models.Lesson.objects.get(pk=lesson_id)
+
 
 
         try:
@@ -101,6 +111,7 @@ class LikeAPIview(APIView):
     
 
 class SendMailAPI(APIView):
+    permission_classes = [permissions.IsAdminUser]
     """Sending news to users' email"""
     def get(self, request):
         serializer = serializers.MailSerializer()
